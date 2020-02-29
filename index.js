b83460f74ef5e167d4d32841466693ba4b4fbff1
raj-vora <raj.jv@somaiya.edu>
2020-02-29 21:36:57 +0530

    phonebook backend step5

--- index.js
+++ index.js
@@ -5,60 +5,79 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 
-let persons = [
-	{
-	  	"name": "Arto Hellas",
-	  	"number": "040-123456",
-	  	"id": 1
-	},
-	{
-	  	"name": "Ada Lovelace",
-	  	"number": "39-44-5323523",
-	  	"id": 2
-	},
-	{
-		 "name": "Dan Abramov",
-	  	"number": "12-43-234345",
-	  	"id": 3
-	},
-	{
-		"name": "Mary Poppendieck",
-		"number": "39-23-6423122",
-		"id": 4
-	}
+let persons = [{
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    }
 ]
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.ceil(Math.random() * Math.pow(10, 3))
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 app.get('/api/persons', (request, response) => {
-	response.json(persons)
+    response.json(persons)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	const person = persons.find(person => person.id === id)
-	if(person) {
-		response.json(person)
-	} else{
-		response.status(404).end()
-	}
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
-app.delete('/api/persons/:id', (request, response) =>{
-	const id = Number(request.params.id)
-	const persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
 
-	response.status(204).end()
+    response.status(204).end()
 })
 
 app.get('/info', (request, response) => {
-	const message = `<p>Phonebook has info for ${persons.length} people</p>` + (new Date().toString())
-	response.send(message)
+    const message = `<p>Phonebook has info for ${persons.length} people</p>` + (new Date().toString())
+    response.send(message)
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
