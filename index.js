c2c8478be57cefc12f03beeb05127139d1ea370a
raj-vora <raj.jv@somaiya.edu>
2020-03-01 19:58:19 +0530

    phonebook database step2

--- index.js
+++ index.js
@@ -56,13 +56,9 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id).then(person => {
+        response.json(person.toJSON())
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
